886da5f2689e77b7faa15a14122db9d4797c8c2e
badjoke-lab <badjoke.lab@gmail.com>
2026-08-17 00:34:32 +0900

    Index Compare route

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ import { SITE_URL } from '../lib/site'
 export const dynamic = 'force-static'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const paths = ['', '/hardware', '/software', '/incidents', '/methodology', '/about', '/support']
+  const paths = ['', '/hardware', '/software', '/incidents', '/compare', '/methodology', '/about', '/support']
   return [
     ...paths.map((path) => ({ url: `${SITE_URL}${path}/` })),
     ...entities.map((entity) => ({ url: `${SITE_URL}/wallet/${entity.slug}/` })),
